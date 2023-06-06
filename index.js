070d76e8dff72efd588dd1401e2c995e4041188b
Safaid Ansari <safaidansari07@gmail.com>
2023-06-06 19:29:51 +0530

    Add real time functionality with socket.io

--- index.js
+++ index.js
@@ -16,9 +16,56 @@ app.use(cors());
 app.use("/api/user", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/message", messageRoute);
-
+app.get("/", (req, res) => {
+  res.send("Hey there");
+});
 app.use(notFound);
 app.use(errorHandler);
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`our server listening on port", ${PORT}`.yellow.bold);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 6000,
+  cors: {
+    origin: "http://localhost:8001",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to IO socket");
+
+  socket.on("setup", (user) => {
+    socket.join(user._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room", room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat user not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(user._id);
+  });
 });
